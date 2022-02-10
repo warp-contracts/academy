7ad23a288d19613fb6af8bb36ecf97a782d90fd0
asiaziola <ziola.jm@gmail.com>
2022-02-10 11:16:35 +0100

    fix: removing lib folder

--- lib/types/contracts/actions/read/balance.d.ts
+++
@@ -1,9 +0,0 @@
-import { PstAction, PstState } from '../../../contracts/types/types';
-export declare const balance: (state: PstState, { input: { target } }: PstAction) => Promise<{
-    result: {
-        target: string;
-        ticker: string;
-        balance: number;
-    };
-}>;
-//# sourceMappingURL=balance.d.ts.map
--- lib/types/contracts/actions/write/transferTokens.d.ts
+++
@@ -1,5 +0,0 @@
-import { PstAction, PstState } from '../../../contracts/types/types';
-export declare const transferTokens: (state: PstState, { caller, input: { target, qty } }: PstAction) => Promise<{
-    state: PstState;
-}>;
-//# sourceMappingURL=transferTokens.d.ts.map
