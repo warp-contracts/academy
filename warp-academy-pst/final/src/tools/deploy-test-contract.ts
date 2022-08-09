90d68db8509d87f2e10dae8be03591a800a8ce13
asiaziola <ziola.jm@gmail.com>
2022-08-09 14:21:36 +0200

    fix: restore deploy test pst contract

--- warp-academy-pst/final/src/tools/deploy-test-contract.ts
+++ warp-academy-pst/final/src/tools/deploy-test-contract.ts
@@ -25,37 +25,30 @@ let warp: Warp;
   LoggerFactory.INST.logLevel('error');
 
   warp = WarpNodeFactory.memCachedBased(arweave).useArweaveGateway().build();
-  // wallet = await arweave.wallets.generate();
-  // const address = await arweave.wallets.getAddress(wallet);
-  // await arweave.api.get(`/mint/${address}/1000000000000000`);
-  // walletAddress = await arweave.wallets.jwkToAddress(wallet);
+  wallet = await arweave.wallets.generate();
+  const address = await arweave.wallets.getAddress(wallet);
+  await arweave.api.get(`/mint/${address}/1000000000000000`);
+  walletAddress = await arweave.wallets.jwkToAddress(wallet);
 
-  // contractSrc = fs.readFileSync(path.join(__dirname, '../../dist/contract.js'), 'utf8');
-  // const stateFromFile: PstState = JSON.parse(
-  //   fs.readFileSync(path.join(__dirname, '../../dist/contracts/initial-state.json'), 'utf8')
-  // );
+  contractSrc = fs.readFileSync(path.join(__dirname, '../../dist/contract.js'), 'utf8');
+  const stateFromFile: PstState = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../../dist/contracts/initial-state.json'), 'utf8')
+  );
 
-  // initialState = {
-  //   ...stateFromFile,
-  //   ...{
-  //     owner: walletAddress,
-  //   },
-  // };
+  initialState = {
+    ...stateFromFile,
+    ...{
+      owner: walletAddress,
+    },
+  };
 
-  // const contractTxId = await warp.createContract.deploy({
-  //   wallet,
-  //   initState: JSON.stringify(initialState),
-  //   src: contractSrc,
-  // });
-
-  // console.log(contractTxId);
-
-  const contract = warp.contract('lKw1ihNCLy-lTh3-RwXelV7-sWGGpRPKpF2mjpBCqNo');
-
-  const result = await contract.viewState({
-    function: 'getSchema',
-    id: 'Human',
+  const contractTxId = await warp.createContract.deploy({
+    wallet,
+    initState: JSON.stringify(initialState),
+    src: contractSrc,
   });
-  console.log(result);
+
+  console.log(contractTxId);
+
   await arweave.api.get('mine');
 })();
