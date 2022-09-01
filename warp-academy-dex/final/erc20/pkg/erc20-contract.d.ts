2f3a348a4a14e2974f4b554755e559bd2f4997c9
asiaziola <ziola.jm@gmail.com>
2022-09-01 20:19:18 +0200

    erc20 wasm files

---
+++ warp-academy-dex/final/erc20/pkg/erc20-contract.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {any} interaction
+* @returns {Promise<any | undefined>}
+*/
+export function handle(interaction: any): Promise<any | undefined>;
+/**
+* @param {any} state
+*/
+export function initState(state: any): void;
+/**
+* @returns {any}
+*/
+export function currentState(): any;
+/**
+* @returns {number}
+*/
+export function version(): number;
+/**
+* @returns {number}
+*/
+export function lang(): number;
